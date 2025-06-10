fecbb1649d496c0e7b23cd96d8ac8a980561c49f
Junsung Choi <choiexe1@gmail.com>
2025-06-10 21:54:58 +0900

    style: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:planee/core/ui/app_text_style.dart';
+import 'package:planee/presentation/components/form_input_field.dart';
+import 'package:planee/presentation/widgets/app_check_box.dart';
+import 'package:planee/presentation/widgets/box_icon.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +22,31 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('상세 일정'), centerTitle: true),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            spacing: 12,
+            children: [
+              AppCheckbox(),
+              BoxIcon(icon: Icon(Icons.timer_sharp)),
+              Center(
+                child: FormInputField(
+                  label: Text(
+                    '제목',
+                    style: AppTextStyle.subTitle1.copyWith(
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  controller: TextEditingController(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
