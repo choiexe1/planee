9c412ce8d10ff22c55dde12af5130c8fd4890186
Junsung Choi <choiexe1@gmail.com>
2025-06-11 22:21:07 +0900

    feat: local event repository 구현체

---
+++ lib/data/repositories/local_event_repository.dart
@@ -0,0 +1,27 @@
+import 'package:planee/data/dto/event_dto.dart';
+import 'package:planee/data/mapper/event_mapper.dart';
+import 'package:planee/domain/data_sources/event_data_source.dart';
+import 'package:planee/domain/entities/event_entity.dart';
+import 'package:planee/domain/repositories/event_repository.dart';
+
+class LocalEventRepository implements EventRepository {
+  const LocalEventRepository(this._dataSource);
+
+  final EventDataSource _dataSource;
+
+  @override
+  Future<List<EventEntity>> findAll() async {
+    final dtos = await _dataSource.findAll();
+    return dtos.map((dto) => dto.toEntity()).toList();
+  }
+
+  @override
+  Future<EventEntity> findOne(int id) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> create(EventDTO dto) async {
+    await _dataSource.create(dto);
+  }
+}
