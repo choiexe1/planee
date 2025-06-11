10687eea3e948365b7820e1e885aca0b3748ebbb
Junsung Choi <choiexe1@gmail.com>
2025-06-11 22:19:58 +0900

    feat: createEvent 관련 data source, repository, view model 인젝션

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -1,12 +1,30 @@
 import 'package:get_it/get_it.dart';
 import 'package:planee/core/db/sqlite_database.dart';
+import 'package:planee/data/data_sources/local_event_data_source.dart';
+import 'package:planee/data/repositories/local_event_repository.dart';
+import 'package:planee/domain/data_sources/event_data_source.dart';
+import 'package:planee/domain/repositories/event_repository.dart';
+import 'package:planee/presentation/view_models/create_event_view_model.dart';
 import 'package:sqflite/sqlite_api.dart';
 
 GetIt sl = GetIt.instance;
 
 void injection() {
-  // App Core
-  sl.registerSingletonAsync<Database>(
-    () => SQLiteDatabase().init(),
-  );
+  sl
+    ..registerSingletonAsync<Database>(
+      () => SQLiteDatabase().init(),
+    )
+    // Data Sources
+    ..registerLazySingleton<EventDataSource>(
+      () => LocalEventDataSource(sl()),
+    )
+    // Repositories
+    ..registerLazySingleton<EventRepository>(
+      () => LocalEventRepository(sl()),
+    )
+    // Use Cases
+    // View Models
+    ..registerLazySingleton<CreateEventViewModel>(
+      () => CreateEventViewModel(sl()),
+    );
 }
