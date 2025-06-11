aebd1c146bfce01c386d9e33aed308ab9ed52b63
Junsung Choi <choiexe1@gmail.com>
2025-06-11 19:58:51 +0900

    feat: detail field를 단순화

--- lib/presentation/components/detail_field.dart
+++ lib/presentation/components/detail_field.dart
@@ -1,65 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:planee/core/ui/app_color.dart';
-import 'package:planee/presentation/widgets/box_icon.dart';
-
-enum DetailFieldVariant { simple, detailed }
 
 class DetailField extends StatelessWidget {
-  final Text title;
-  final Text description;
-  final Icon icon;
-  final DetailFieldVariant variant;
-  final bool? editable;
-  final Text? description2;
-
-  DetailField._internal({
-    super.key,
+  const DetailField({
     required this.title,
-    required this.description,
-    required this.icon,
-    required this.variant,
+    required this.children,
+    super.key,
     this.editable = false,
-    this.description2,
-  }) {
-    if (variant == DetailFieldVariant.detailed && description2 == null) {
-      throw Exception(
-        'DetailField.detailed는 반드시 description2 인자를 초기화해야합니다. 현재 description2 = $description2',
-      );
-    }
-  }
+  });
 
-  DetailField.simple({
-    Key? key,
-    required Text title,
-    required Text description,
-    required Icon icon,
-    bool? editable = false,
-  }) : this._internal(
-         key: key,
-         title: title,
-         editable: editable,
-         description: description,
-         icon: icon,
-         variant: DetailFieldVariant.simple,
-         description2: null,
-       );
-
-  DetailField.detailed({
-    Key? key,
-    required Text title,
-    required Text description,
-    required Icon icon,
-    bool? editable = false,
-    Text? description2,
-  }) : this._internal(
-         key: key,
-         title: title,
-         editable: editable,
-         description: description,
-         icon: icon,
-         variant: DetailFieldVariant.detailed,
-         description2: description2,
-       );
+  final Text title;
+  final List<Widget> children;
+  final bool editable;
 
   @override
   Widget build(BuildContext context) {
@@ -68,29 +19,15 @@ class DetailField extends StatelessWidget {
       spacing: 8,
       children: [
         Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          spacing: 12,
           children: [
             title,
-            editable != null
-                ? Icon(Icons.edit, color: AppColor.black, size: 20)
-                : SizedBox(),
+            if (editable) ...[
+              const SizedBox(width: 8),
+              const Icon(Icons.edit, size: 18),
+            ],
           ],
         ),
-
-        Row(
-          spacing: 12,
-          children: [
-            BoxIcon(icon: icon),
-            if (variant == DetailFieldVariant.simple) ...[
-              description,
-            ] else
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [description, description2!],
-              ),
-          ],
-        ),
+        Row(children: children),
       ],
     );
   }
