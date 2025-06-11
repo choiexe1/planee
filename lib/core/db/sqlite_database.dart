0223fe0b7f56cd83744a08dc99a016350e31254f
Junsung Choi <choiexe1@gmail.com>
2025-06-11 20:55:05 +0900

    feat: sqlite database

---
+++ lib/core/db/sqlite_database.dart
@@ -0,0 +1,24 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SQLiteDatabase {
+  Future<Database> init() async {
+    final path = join(await getDatabasesPath(), 'planee.db');
+
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          '''
+          CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL
+          )
+        ''',
+        );
+      },
+    );
+  }
+}
