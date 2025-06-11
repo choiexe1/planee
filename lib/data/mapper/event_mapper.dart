4ea7c8134f75f8d09eccfb662d9d5eb81857e175
Junsung Choi <choiexe1@gmail.com>
2025-06-11 22:23:20 +0900

    feat: event mapper

---
+++ lib/data/mapper/event_mapper.dart
@@ -0,0 +1,14 @@
+import 'package:planee/data/dto/event_dto.dart';
+import 'package:planee/domain/entities/event_entity.dart';
+
+extension EventMapper on EventDTO {
+  EventEntity toEntity() {
+    return EventEntity(
+      title: title,
+      description: description,
+      id: id!,
+      eventTime: DateTime.now(),
+      createdAt: DateTime.now(),
+    );
+  }
+}
