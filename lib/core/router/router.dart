8847dc6a17c652cc3a55a05e48dd9e09e9ae7035
Junsung Choi <choiexe1@gmail.com>
2025-06-11 22:19:25 +0900

    feat: createEventScreenRoot viewmodel 인젝션

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:planee/core/router/routes.dart';
-import 'package:planee/presentation/screens/create_event/create_event_screen.dart';
+import 'package:planee/core/service_locator.dart';
+import 'package:planee/presentation/screens/create_event/create_event_screen_root.dart';
+import 'package:planee/presentation/view_models/create_event_view_model.dart';
 
 GoRouter appRouter = GoRouter(
   initialLocation: Routes.createEvent,
@@ -9,7 +11,7 @@ GoRouter appRouter = GoRouter(
     GoRoute(
       path: Routes.createEvent,
       builder: (BuildContext context, GoRouterState state) {
-        return const CreateEventScreen();
+        return CreateEventScreenRoot(viewModel: sl<CreateEventViewModel>());
       },
     ),
   ],
