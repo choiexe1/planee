9db9d4b8ec797c7501494ed150d4206fb0478034
Junsung Choi <choiexe1@gmail.com>
2025-06-11 20:55:24 +0900

    feat: sqlite databases connection instance di

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -1,5 +1,12 @@
 import 'package:get_it/get_it.dart';
+import 'package:planee/core/db/sqlite_database.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 GetIt sl = GetIt.instance;
 
-void injection() {}
+void injection() {
+  // App Core
+  sl.registerSingletonAsync<Database>(
+    () => SQLiteDatabase().init(),
+  );
+}
