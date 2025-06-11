e51872a9730eeb44c816599f1afcffc224657f63
Junsung Choi <choiexe1@gmail.com>
2025-06-11 14:25:07 +0900

    feat: add grey

--- lib/core/ui/app_color.dart
+++ lib/core/ui/app_color.dart
@@ -5,5 +5,6 @@ abstract class AppColor {
   static const Color black = Color(0xFF1F1F1F);
   static const Color blue = Color(0xFF3384FF);
   static const Color input = Color(0xFFF0F2F5);
+  static const Color grey = Color(0xFF8C8C8C);
   static const Color caption = Color(0xFF121212);
 }
