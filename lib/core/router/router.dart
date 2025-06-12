7bcd8aaaa279e018afa10e10c65f1f080235f7bb
Junsung Choi <choiexe1@gmail.com>
2025-06-12 22:13:25 +0900

    feat: home screen 추가, 이니셜 라우트를 홈으로 변경

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -3,11 +3,18 @@ import 'package:go_router/go_router.dart';
 import 'package:planee/core/router/routes.dart';
 import 'package:planee/core/service_locator.dart';
 import 'package:planee/presentation/screens/create_event/create_event_screen_root.dart';
+import 'package:planee/presentation/screens/home/home_screen.dart';
 import 'package:planee/presentation/view_models/create_event_view_model.dart';
 
 GoRouter appRouter = GoRouter(
-  initialLocation: Routes.createEvent,
+  initialLocation: Routes.home,
   routes: [
+    GoRoute(
+      path: Routes.home,
+      builder: (BuildContext context, GoRouterState state) {
+        return HomeScreen();
+      },
+    ),
     GoRoute(
       path: Routes.createEvent,
       builder: (BuildContext context, GoRouterState state) {
