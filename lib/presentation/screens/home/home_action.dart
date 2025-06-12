2ec3285f471cefdfd539511584dae339dff81b2c
Junsung Choi <choiexe1@gmail.com>
2025-06-13 03:30:22 +0900

    feat: longPress 액션 추가

--- lib/presentation/screens/home/home_action.dart
+++ lib/presentation/screens/home/home_action.dart
@@ -7,4 +7,5 @@ sealed class HomeAction with _$HomeAction {
   const factory HomeAction.onTapPrevious() = OnTapPrevious;
   const factory HomeAction.onTapNext() = OnTapNext;
   const factory HomeAction.onTapDate(DateTime date) = OnTapDate;
+  const factory HomeAction.onLongPressDate(DateTime date) = OnLongPressDate;
 }
