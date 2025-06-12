c6f756e39878e7e07bbbe683edc87973551bdba5
Junsung Choi <choiexe1@gmail.com>
2025-06-13 03:04:03 +0900

    feat: dateTime extension

--- lib/domain/extension/date_time_extension.dart
+++ lib/domain/extension/date_time_extension.dart
@@ -1,5 +1,5 @@
 extension DateTimeExtension on DateTime {
-  String get toKor => '$year년 $month월 $day일';
+  String get toKor => '$year년 $month월';
 
   String get toEng {
     final monthNames = [
@@ -17,10 +17,33 @@ extension DateTimeExtension on DateTime {
       'December',
     ];
 
-    if (month >= 1 && month <= 12) {
-      return '${monthNames[month - 1]}, $year';
-    } else {
-      throw Exception('올바른 날짜를 입력하세요.');
-    }
+    return '${monthNames[month - 1]} $year';
+  }
+
+  DateTime get previousMonth => DateTime(year, month - 1);
+  DateTime get nextMonth => DateTime(year, month + 1);
+
+  /// [firstWeekDayOfMonth]는 해당하는 월의 첫째 요일을 [int] 형태로 반환한다.
+  /// 월요일(1) ~ 일요일(7)
+  int get firstWeekDayOfMonth => DateTime(year, month).weekday;
+
+  /// [week]에 따라 계산할 범위가 정해진다. 불변 리스트 형태로 반환된다.
+  /// example: calendarDates(5)는 5주간의 날짜(7 * 5)를 계산한다.
+  List<DateTime> calculateCalendarDates(int week) {
+    final dateLength = week * 7;
+
+    final int daysToSubtract = firstWeekDayOfMonth % 7;
+
+    final DateTime calendarStartDate = DateTime(year, month).subtract(
+      Duration(days: daysToSubtract),
+    );
+
+    final List<DateTime> dates = List.generate(
+      dateLength,
+      (index) => calendarStartDate.add(Duration(days: index)),
+      growable: false,
+    );
+
+    return List.unmodifiable(dates);
   }
 }
