8e84a43961016c5c09689837b2c350e31515e323
Junsung Choi <choiexe1@gmail.com>
2025-06-13 03:09:27 +0900

    feat: home action

---
+++ lib/presentation/screens/home/home_action.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_action.freezed.dart';
+
+@freezed
+sealed class HomeAction with _$HomeAction {
+  const factory HomeAction.onTapPrevious() = OnTapPrevious;
+  const factory HomeAction.onTapNext() = OnTapNext;
+  const factory HomeAction.onTapDate(DateTime date) = OnTapDate;
+}
