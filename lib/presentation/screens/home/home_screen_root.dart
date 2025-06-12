5d2b47c747f2d2949740263d9453b90517bfc6cb
Junsung Choi <choiexe1@gmail.com>
2025-06-13 03:09:31 +0900

    feat: home screen root

---
+++ lib/presentation/screens/home/home_screen_root.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:planee/presentation/screens/home/home_action.dart';
+import 'package:planee/presentation/screens/home/home_screen.dart';
+import 'package:planee/presentation/view_models/home_view_model.dart';
+
+class HomeScreenRoot extends StatefulWidget {
+  const HomeScreenRoot({required this.viewModel, super.key});
+
+  final HomeViewModel viewModel;
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenRootState();
+}
+
+class _HomeScreenRootState extends State<HomeScreenRoot> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.viewModel,
+      builder: (context, _) {
+        return HomeScreen(
+          state: widget.viewModel.state,
+          onAction: (HomeAction action) {
+            switch (action) {
+              case OnTapPrevious():
+                widget.viewModel.onAction(action);
+              case OnTapNext():
+                widget.viewModel.onAction(action);
+              case OnTapDate():
+                widget.viewModel.onAction(action);
+            }
+          },
+        );
+      },
+    );
+  }
+}
