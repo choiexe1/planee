43a6896fc157e98cc8951806020b0f229b57ae17
Junsung Choi <choiexe1@gmail.com>
2025-06-14 16:45:40 +0900

    feat: onTapTitle action

--- lib/presentation/screens/home/home_action.dart
+++ lib/presentation/screens/home/home_action.dart
@@ -6,6 +6,7 @@ part 'home_action.freezed.dart';
 sealed class HomeAction with _$HomeAction {
   const factory HomeAction.onTapPrevious() = OnTapPrevious;
   const factory HomeAction.onTapNext() = OnTapNext;
-  const factory HomeAction.onTapDate(DateTime date) = OnTapDate;
-  const factory HomeAction.onLongPressDate(DateTime date) = OnLongPressDate;
+  const factory HomeAction.onTapDate(DateTime dateTime) = OnTapDate;
+  const factory HomeAction.onLongPressDate(DateTime dateTime) = OnLongPressDate;
+  const factory HomeAction.onTapTitle(DateTime dateTime) = OnTapTitle;
 }
