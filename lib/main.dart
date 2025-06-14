b6c629bbea5ce20f8ca826159b72ff4bae4f9cff
Junsung Choi <choiexe1@gmail.com>
2025-06-14 18:03:10 +0900

    feat: app scaffold 기본 배경화면을 하얀색으로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:planee/core/router/router.dart';
 import 'package:planee/core/service_locator.dart';
+import 'package:planee/core/ui/app_color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +18,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColor.white,
+      ),
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
     );
