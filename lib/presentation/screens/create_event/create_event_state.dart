69380676200e7ee6f67b79b6d2ee04578f5fdd54
Junsung Choi <choiexe1@gmail.com>
2025-06-15 20:01:45 +0900

    feat: create event state

---
+++ lib/presentation/screens/create_event/create_event_state.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'create_event_state.freezed.dart';
+
+@freezed
+abstract class CreateEventState with _$CreateEventState {
+  const factory CreateEventState({
+    @Default(false) bool isLoading,
+    @Default(TimeOfDay(hour: 0, minute: 0)) TimeOfDay timeOfDay,
+  }) = _CreateEventState;
+}
