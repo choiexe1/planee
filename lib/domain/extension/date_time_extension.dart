1ad5ac6d187432908dcf9a593c7c026c9ab0cf42
Junsung Choi <choiexe1@gmail.com>
2025-06-15 19:57:27 +0900

    feat: date time extension에 편의 게터 추가

--- lib/domain/extension/date_time_extension.dart
+++ lib/domain/extension/date_time_extension.dart
@@ -1,6 +1,45 @@
 extension DateTimeExtension on DateTime {
   String get toKor => '$year년 $month월';
 
+  bool get isWeekend =>
+      weekday == DateTime.saturday || weekday == DateTime.sunday;
+  bool get isSaturday => weekday == DateTime.saturday;
+  bool get isSunday => weekday == DateTime.sunday;
+
+  String get dateString => '$year년, $month월 $day일';
+
+  String get eventTimeAsString {
+    if (hour == 0) {
+      return '시간이 지정되지 않았습니다.';
+    }
+
+    final formattedHour = hour % 12 == 0 ? 12 : hour % 12;
+    final hourString = formattedHour.toString().padLeft(2, '0');
+
+    return minute == 0 ? '$hourString시' : '$hourString시 $minute분';
+  }
+
+  String get weekDayAsKorean {
+    switch (weekday) {
+      case 1:
+        return '월';
+      case 2:
+        return '화';
+      case 3:
+        return '수';
+      case 4:
+        return '목';
+      case 5:
+        return '금';
+      case 6:
+        return '토';
+      case 7:
+        return '일';
+      default:
+        return '';
+    }
+  }
+
   String get toEng {
     final monthNames = [
       'January',
