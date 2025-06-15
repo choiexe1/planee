04f03fa994c3f2c94e994a956b52db6ebb86eb21
Junsung Choi <choiexe1@gmail.com>
2025-06-15 20:02:20 +0900

    feat: 시간 설정 기능

--- lib/presentation/view_models/create_event_view_model.dart
+++ lib/presentation/view_models/create_event_view_model.dart
@@ -1,20 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:planee/domain/use_cases/create_event_use_case.dart';
 import 'package:planee/presentation/screens/create_event/create_event_action.dart';
+import 'package:planee/presentation/screens/create_event/create_event_state.dart';
 
 class CreateEventViewModel with ChangeNotifier {
   CreateEventViewModel(this._createEventUseCase);
 
+  CreateEventState _state = const CreateEventState();
+
   final CreateEventUseCase _createEventUseCase;
+
+  CreateEventState get state => _state;
 
   Future<void> onAction(CreateEventAction action) async {
     switch (action) {
       case SaveEvent(:final String title, :final String description):
         await _createEvent(title, description);
+      case ChangeTime(:final TimeOfDay timeOfDay):
+        await _changeTime(timeOfDay);
     }
   }
 
   Future<void> _createEvent(String title, String description) async {
     await _createEventUseCase.execute(title, description);
+  }
+
+  Future<void> _changeTime(TimeOfDay timeOfDay) async {
+    _state = _state.copyWith(timeOfDay: timeOfDay);
+    notifyListeners();
   }
 }
