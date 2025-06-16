dd1f710d53436636c06c7dcd43ddeceb4dbabcca
Junsung Choi <choiexe1@gmail.com>
2025-06-16 20:31:15 +0900

    feat: time picker size 조절

--- lib/presentation/components/time_picker.dart
+++ lib/presentation/components/time_picker.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:planee/core/ui/app_color.dart';
+import 'package:planee/core/ui/app_text_style.dart';
 import 'package:planee/presentation/widgets/scrollable_picker.dart';
+import 'package:planee/presentation/widgets/tap_button.dart';
 
 class TimePicker extends StatefulWidget {
   const TimePicker({
     required this.initialTime,
-    required this.onTimeChanged,
+    required this.onTapDone,
     super.key,
   });
 
   final TimeOfDay initialTime;
-  final ValueChanged<TimeOfDay> onTimeChanged;
+  final ValueChanged<TimeOfDay> onTapDone;
 
   @override
   State<TimePicker> createState() => _TimePickerState();
@@ -55,77 +57,95 @@ class _TimePickerState extends State<TimePicker> {
       _selectedHour = newHour;
       _selectedMinute = newMinute;
     });
-
-    widget.onTimeChanged(
-      TimeOfDay(hour: _selectedHour, minute: _selectedMinute),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        SizedBox(
-          width: 60,
-          child: ScrollablePicker(
-            controller: _hourController,
-            items: _hours
-                .map((hour) => hour.toString().padLeft(2, '0'))
-                .toList(),
-            selectedItem: _hours.indexOf(_selectedHour),
-            onSelectedItemChanged: (index) {
-              _onSelectionChanged();
-            },
-            itemBuilder: (context, index) {
-              final hour = _hours[index];
-              return Center(
-                child: Text(
-                  hour.toString().padLeft(2, '0'),
-                  style: TextStyle(
-                    fontSize: hour == _selectedHour ? 22 : 18,
-                    fontWeight: hour == _selectedHour
-                        ? FontWeight.bold
-                        : FontWeight.normal,
-                    color: hour == _selectedHour
-                        ? AppColor.blue
-                        : AppColor.black,
-                  ),
-                ),
-              );
-            },
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 60,
+              height: 180,
+              child: ScrollablePicker(
+                controller: _hourController,
+                items: _hours
+                    .map((hour) => hour.toString().padLeft(2, '0'))
+                    .toList(),
+                selectedItem: _hours.indexOf(_selectedHour),
+                onSelectedItemChanged: (index) {
+                  _onSelectionChanged();
+                },
+                itemBuilder: (context, index) {
+                  final hour = _hours[index];
+                  return Center(
+                    child: Text(
+                      hour.toString().padLeft(2, '0'),
+                      style: TextStyle(
+                        fontSize: hour == _selectedHour ? 22 : 18,
+                        fontWeight: hour == _selectedHour
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color: hour == _selectedHour
+                            ? AppColor.blue
+                            : AppColor.black,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(width: 16),
+            SizedBox(
+              width: 60,
+              height: 180,
+              child: ScrollablePicker(
+                controller: _minuteController,
+                items: _minutes
+                    .map((minute) => minute.toString().padLeft(2, '0'))
+                    .toList(),
+                selectedItem: _minutes.indexOf(_selectedMinute),
+                onSelectedItemChanged: (index) {
+                  _onSelectionChanged();
+                },
+                itemBuilder: (context, index) {
+                  final minute = _minutes[index];
+                  return Center(
+                    child: Text(
+                      minute.toString().padLeft(2, '0'),
+                      style: TextStyle(
+                        fontSize: minute == _selectedMinute ? 22 : 18,
+                        fontWeight: minute == _selectedMinute
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        color: minute == _selectedMinute
+                            ? AppColor.blue
+                            : AppColor.black,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        const SizedBox(width: 16),
-        SizedBox(
-          width: 60,
-          child: ScrollablePicker(
-            controller: _minuteController,
-            items: _minutes
-                .map((minute) => minute.toString().padLeft(2, '0'))
-                .toList(),
-            selectedItem: _minutes.indexOf(_selectedMinute),
-            onSelectedItemChanged: (index) {
-              _onSelectionChanged();
-            },
-            itemBuilder: (context, index) {
-              final minute = _minutes[index];
-              return Center(
-                child: Text(
-                  minute.toString().padLeft(2, '0'),
-                  style: TextStyle(
-                    fontSize: minute == _selectedMinute ? 22 : 18,
-                    fontWeight: minute == _selectedMinute
-                        ? FontWeight.bold
-                        : FontWeight.normal,
-                    color: minute == _selectedMinute
-                        ? AppColor.blue
-                        : AppColor.black,
-                  ),
-                ),
-              );
-            },
+        const SizedBox(height: 24),
+        TapButton(
+          name: Text(
+            '완료',
+            style: AppTextStyle.body.copyWith(
+              color: AppColor.white,
+            ),
           ),
+          onTap: () {
+            widget.onTapDone(
+              TimeOfDay(hour: _selectedHour, minute: _selectedMinute),
+            );
+            Navigator.pop(context);
+          },
         ),
       ],
     );
