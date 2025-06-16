6722e5156a1ec28bb6086ec2fe33b410cd6bedb2
Junsung Choi <choiexe1@gmail.com>
2025-06-16 19:14:43 +0900

    feat: find upcoming events use case di

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -5,6 +5,7 @@ import 'package:planee/data/repositories/local_event_repository.dart';
 import 'package:planee/domain/data_sources/event_data_source.dart';
 import 'package:planee/domain/repositories/event_repository.dart';
 import 'package:planee/domain/use_cases/create_event_use_case.dart';
+import 'package:planee/domain/use_cases/find_upcoming_events_use_case.dart';
 import 'package:planee/presentation/view_models/create_event_view_model.dart';
 import 'package:planee/presentation/view_models/home_view_model.dart';
 import 'package:sqflite/sqlite_api.dart';
@@ -26,11 +27,14 @@ void injection() {
     )
     // Use Cases
     ..registerLazySingleton<CreateEventUseCase>(() => CreateEventUseCase(sl()))
+    ..registerLazySingleton<FindUpcomingEventsUseCase>(
+      () => FindUpcomingEventsUseCase(sl()),
+    )
     // View Models
     ..registerFactory<CreateEventViewModel>(
       () => CreateEventViewModel(sl()),
     )
-    ..registerLazySingleton<HomeViewModel>(
-      HomeViewModel.new,
+    ..registerFactory<HomeViewModel>(
+      () => HomeViewModel(sl()),
     );
 }
