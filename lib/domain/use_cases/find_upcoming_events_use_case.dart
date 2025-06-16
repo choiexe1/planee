e1a5b7664ecc569c1f1bf8d9f71aaf94f133b263
Junsung Choi <choiexe1@gmail.com>
2025-06-16 19:14:28 +0900

    feat: find upcoming events use case

---
+++ lib/domain/use_cases/find_upcoming_events_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:planee/domain/entities/event_entity.dart';
+import 'package:planee/domain/repositories/event_repository.dart';
+
+class FindUpcomingEventsUseCase {
+  const FindUpcomingEventsUseCase(this._eventRepository);
+
+  final EventRepository _eventRepository;
+
+  Future<List<EventEntity>> execute(DateTime start, DateTime end) {
+    return _eventRepository.findUpcomingEvents(start, end);
+  }
+}
