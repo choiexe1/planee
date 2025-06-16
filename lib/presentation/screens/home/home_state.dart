00d1cdf896579cef08b9abef505136c8a3043f8c
Junsung Choi <choiexe1@gmail.com>
2025-06-16 19:15:02 +0900

    feat: upcoming events state

--- lib/presentation/screens/home/home_state.dart
+++ lib/presentation/screens/home/home_state.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:planee/domain/entities/event_entity.dart';
 
 part 'home_state.freezed.dart';
 
@@ -9,5 +10,6 @@ abstract class HomeState with _$HomeState {
     @Default(null) List<DateTime>? calendar,
     @Default(null) DateTime? selectedDate,
     @Default(null) DateTime? currentDisplayMonth,
+    @Default([]) List<EventEntity> upcomingEvents,
   }) = _HomeState;
 }
