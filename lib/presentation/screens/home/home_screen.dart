121dbf6dc9261810ab4212ae0e0851bb8ece7ae7
Junsung Choi <choiexe1@gmail.com>
2025-06-16 20:34:48 +0900

    feat: 다가오는 일정 섹션 추가

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:planee/core/ui/app_color.dart';
+import 'package:planee/core/ui/app_text_style.dart';
 import 'package:planee/presentation/components/calendar.dart';
+import 'package:planee/presentation/components/detail_field.dart';
+import 'package:planee/presentation/components/upcoming_event_tile.dart';
 import 'package:planee/presentation/screens/home/home_action.dart';
 import 'package:planee/presentation/screens/home/home_state.dart';
 
@@ -18,23 +22,47 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child: Column(
               children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: Calendar(
-                    currentDisplayMonth:
-                        state.currentDisplayMonth ?? DateTime.now(),
-                    dates: state.calendar ?? [],
-                    selectedDate: state.selectedDate ?? DateTime.now(),
-                    onTapTitle: (DateTime dateTime) =>
-                        onAction(HomeAction.onTapTitle(dateTime)),
-                    onTapPrevious: () =>
-                        onAction(const HomeAction.onTapPrevious()),
-                    onTapNext: () => onAction(const HomeAction.onTapNext()),
-                    onTapDate: (DateTime dateTime) =>
-                        onAction(HomeAction.onTapDate(dateTime)),
-                    onLongPressDate: (DateTime dateTime) =>
-                        onAction(HomeAction.onLongPressDate(dateTime)),
+                Calendar(
+                  currentDisplayMonth:
+                      state.currentDisplayMonth ?? DateTime.now(),
+                  dates: state.calendar ?? [],
+                  selectedDate: state.selectedDate ?? DateTime.now(),
+                  onTapTitle: (DateTime dateTime) =>
+                      onAction(HomeAction.onTapTitle(dateTime)),
+                  onTapPrevious: () =>
+                      onAction(const HomeAction.onTapPrevious()),
+                  onTapNext: () => onAction(const HomeAction.onTapNext()),
+                  onTapDate: (DateTime dateTime) =>
+                      onAction(HomeAction.onTapDate(dateTime)),
+                  onLongPressDate: (DateTime dateTime) =>
+                      onAction(HomeAction.onLongPressDate(dateTime)),
+                ),
+                DetailField(
+                  title: Text(
+                    '다가오는 일정',
+                    style: AppTextStyle.subTitle1.copyWith(
+                      color: AppColor.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  children: [
+                    if (state.upcomingEvents.isNotEmpty)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ...state.upcomingEvents.map(
+                            (event) => UpcomingEventTile(event: event),
+                          ),
+                        ],
+                      )
+                    else
+                      Text(
+                        '다가오는 일정이 없어요!',
+                        style: AppTextStyle.body.copyWith(
+                          color: AppColor.grey,
+                        ),
+                      ),
+                  ],
                 ),
               ],
             ),
