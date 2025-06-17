63329d47081dfc9e564b0068a1a883a2d4598eba
Junsung Choi <choiexe1@gmail.com>
2025-06-17 18:47:04 +0900

    feat: form input field 위젯

---
+++ lib/presentation/widgets/form_input_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:planee/core/ui/app_color.dart';
+import 'package:planee/core/ui/app_text_style.dart';
+
+class FormInputField extends StatelessWidget {
+  const FormInputField({required this.validator, super.key});
+
+  final String? Function(String? value) validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        filled: true,
+        contentPadding: const EdgeInsets.all(16),
+        fillColor: AppColor.input,
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColor.red),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColor.red),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        errorStyle: AppTextStyle.body.copyWith(color: AppColor.red),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+      ),
+      style: AppTextStyle.body.copyWith(color: AppColor.black),
+      validator: validator,
+    );
+  }
+}
