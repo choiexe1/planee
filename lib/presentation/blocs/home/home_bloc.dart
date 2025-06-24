b0cbaaba1ac964dcd668ffde5001266f719d963a
Junsung Choi <choiexe1@gmail.com>
2025-06-24 18:20:19 +0900

    feat: home bloc

---
+++ lib/presentation/blocs/home/home_bloc.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:planee/domain/extension/date_time_extension.dart';
+import 'package:planee/domain/use_cases/find_upcoming_events_use_case.dart';
+import 'package:planee/presentation/blocs/home/home_event.dart';
+import 'package:planee/presentation/screens/home/home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc(this._findUpcomingEventsUseCase) : super(HomeState.init()) {
+    on<HomeInitialize>(_init);
+    on<HomeOnTapNext>(_nextMonth);
+    on<HomeOnTapPrevious>(_previousMonth);
+    on<HomeOnTapDate>(_selectDate);
+  }
+
+  final FindUpcomingEventsUseCase _findUpcomingEventsUseCase;
+
+  Future<void> _init(HomeInitialize event, Emitter<HomeState> emit) async {
+    final now = DateTime.now();
+
+    final upcomingEvents = await _findUpcomingEventsUseCase.execute(
+      start: DateTime(now.year, now.month, now.day, now.hour, now.minute),
+      end: DateTime(now.year, now.month, now.day + 1),
+      limit: 5,
+    );
+
+    emit(state.copyWith(upcomingEvents: upcomingEvents));
+  }
+
+  void _nextMonth(HomeOnTapNext event, Emitter<HomeState> emit) {
+    final nextMonth = state.currentDisplayMonth.nextMonth;
+
+    emit(
+      state.copyWith(
+        currentDisplayMonth: nextMonth,
+        calendar: nextMonth.calculateCalendarDates(),
+      ),
+    );
+  }
+
+  void _previousMonth(HomeOnTapPrevious event, Emitter<HomeState> emit) {
+    final previousMonth = state.currentDisplayMonth.previousMonth;
+
+    emit(
+      state.copyWith(
+        currentDisplayMonth: previousMonth,
+        calendar: previousMonth.calculateCalendarDates(),
+      ),
+    );
+  }
+
+  void _selectDate(HomeOnTapDate event, Emitter<HomeState> emit) {
+    emit(state.copyWith(selectedDate: event.dateTime));
+  }
+}
