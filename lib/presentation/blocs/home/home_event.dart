fa61bf60319f1927eed4548e179873be861ab065
Junsung Choi <choiexe1@gmail.com>
2025-06-24 18:20:17 +0900

    feat: home event

---
+++ lib/presentation/blocs/home/home_event.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_event.freezed.dart';
+
+@freezed
+sealed class HomeEvent with _$HomeEvent {
+  const factory HomeEvent.initialize() = HomeInitialize;
+  const factory HomeEvent.onTapPrevious() = HomeOnTapPrevious;
+  const factory HomeEvent.onTapNext() = HomeOnTapNext;
+  const factory HomeEvent.onTapDate(DateTime dateTime) = HomeOnTapDate;
+  const factory HomeEvent.onLongPressDate(DateTime dateTime) =
+      HomeOnLongPressDate;
+  const factory HomeEvent.onTapTitle(DateTime dateTime) =
+      HomeOnTapCalendarTitle;
+}
