3fa265d19be22de29abf6765d1038fef8bb9ecf6
Junsung Choi <choiexe1@gmail.com>
2025-06-24 23:37:34 +0900

    feat: app scaffold 적용하여 바텀 내비게이션바 적용

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -4,17 +4,40 @@ import 'package:go_router/go_router.dart';
 import 'package:planee/core/router/routes.dart';
 import 'package:planee/core/service_locator.dart';
 import 'package:planee/presentation/blocs/create_event/create_event_bloc.dart';
+import 'package:planee/presentation/components/app_scaffold.dart';
 import 'package:planee/presentation/screens/create_event/create_event_screen_root.dart';
 import 'package:planee/presentation/screens/home/home_screen_root.dart';
+import 'package:planee/presentation/screens/settings/settings_screen_root.dart';
 
 GoRouter appRouter = GoRouter(
   initialLocation: Routes.home,
   routes: [
-    GoRoute(
-      path: Routes.home,
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomeScreenRoot();
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return AppScaffold(navigationShell: navigationShell);
       },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (BuildContext context, GoRouterState state) {
+                return const HomeScreenRoot();
+              },
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.settings,
+              builder: (BuildContext context, GoRouterState state) {
+                return SettingsScreenRoot();
+              },
+            ),
+          ],
+        ),
+      ],
     ),
     GoRoute(
       path: Routes.createEvent,
--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,6 +1,7 @@
 abstract class Routes {
   static const String home = '/';
   static const String createEvent = '/event/create/:date';
+  static const String settings = '/settings';
 
   static String createEventWithDate(DateTime date) =>
       '/event/create/${date.toIso8601String()}';
