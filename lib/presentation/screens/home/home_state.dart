ce2c8b632c19bb14866c29047736ddf17e9b6a0d
Junsung Choi <choiexe1@gmail.com>
2025-06-24 18:19:44 +0900

    feat: state 초기값을 런타임에 지정

--- lib/presentation/screens/home/home_state.dart
+++ lib/presentation/screens/home/home_state.dart
@@ -1,15 +1,29 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:planee/domain/entities/event_entity.dart';
+import 'package:planee/domain/extension/date_time_extension.dart';
 
 part 'home_state.freezed.dart';
 
 @freezed
 abstract class HomeState with _$HomeState {
   const factory HomeState({
+    required DateTime currentDisplayMonth,
+    required List<DateTime> calendar,
+    required DateTime selectedDate,
     @Default(false) bool isLoading,
-    @Default(null) List<DateTime>? calendar,
-    @Default(null) DateTime? selectedDate,
-    @Default(null) DateTime? currentDisplayMonth,
     @Default([]) List<EventEntity> upcomingEvents,
   }) = _HomeState;
+
+  const HomeState._();
+
+  factory HomeState.init({int calendarPerWeek = 5}) {
+    final now = DateTime.now();
+
+    return HomeState(
+      currentDisplayMonth: now,
+      selectedDate: now,
+      calendar: now.calculateCalendarDates(week: calendarPerWeek),
+      upcomingEvents: [],
+    );
+  }
 }
